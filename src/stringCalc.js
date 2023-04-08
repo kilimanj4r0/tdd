df1cce918bc0e23c525fdab47c9921d46d75eb18
Vladimir Makharev <v.makharev@yandex.ru>
2023-04-08 11:46:20 +0300

    6 and last waves

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -2,17 +2,21 @@ const calc = (nums) => {
     if (nums === '') {
         return 0;
     }
+    const negatives = nums.match(/-\d+/g);
+    if (negatives) {
+        throw new Error('negatives not allowed');
+    }
     const beginRegex = /^\/\/(.*)\n/;
     let delim = ','; // default
     if (nums.startsWith('//')) {
-        delim = nums.match(beginRegex)[1];
+        [, delim] = nums.match(beginRegex);
         nums = nums.replace(beginRegex, '');
     }
     return nums
         .replace(/\n/g, delim)
         .split(delim)
         .reduce((acc, num) => {
-            return acc + parseInt(num);
+            return num > 1000 ? acc : acc + parseInt(num);
         }, 0);
 };
 
