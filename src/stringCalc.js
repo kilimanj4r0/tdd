58f634fad17f82f916816643c347e088f8eb5b56
Vladimir Makharev <v.makharev@yandex.ru>
2023-04-08 11:42:33 +0300

    5 wave

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -2,9 +2,15 @@ const calc = (nums) => {
     if (nums === '') {
         return 0;
     }
+    const beginRegex = /^\/\/(.*)\n/;
+    let delim = ','; // default
+    if (nums.startsWith('//')) {
+        delim = nums.match(beginRegex)[1];
+        nums = nums.replace(beginRegex, '');
+    }
     return nums
-        .replace(/\n/g, ',')
-        .split(',')
+        .replace(/\n/g, delim)
+        .split(delim)
         .reduce((acc, num) => {
             return acc + parseInt(num);
         }, 0);
