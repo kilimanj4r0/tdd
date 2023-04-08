5999d8b23e8376354e78e999c03242ef42ee56aa
Vladimir Makharev <v.makharev@yandex.ru>
2023-04-08 11:25:55 +0300

    3 and 4 waves

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -2,9 +2,12 @@ const calc = (nums) => {
     if (nums === '') {
         return 0;
     }
-    return nums.split(',').reduce((acc, num) => {
-        return acc + parseInt(num);
-    }, 0);  
+    return nums
+        .replace(/\n/g, ',')
+        .split(',')
+        .reduce((acc, num) => {
+            return acc + parseInt(num);
+        }, 0);
 };
 
 export default calc;
